2f65c20336b5d0c626e7063acd1ffbefbb68a1e1
Mike <shpoopdy@gmail.com>
2022-03-10 17:47:29 -0800

    Check argument

--- findstates.php
+++ findstates.php
@@ -2,9 +2,8 @@
   $states = "California Georgia Indiana Louisiana Minnesota Texas Virginia";
   $statesArray1 = array();
 
-  $regex = "/(.+)nia /i";
+  $regex = "/(.+)nia (/S.+)nia/i";
   if(preg_match($regex, $states, $statesArray1)) {
     echo "<p>$statesArray1[0]</p>";
-    echo "<p>$statesArray1[1]</p>";
   }
 ?>
