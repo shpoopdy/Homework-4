79483a969a7586eb68c45de33a56cae532e69cda
Mike <shpoopdy@gmail.com>
2022-03-10 18:05:57 -0800

    Change argument

--- findstates.php
+++ findstates.php
@@ -3,7 +3,7 @@
   $statesArray1 = array();
 
   $regex = "/(.+)nia /i";
-  $regex2 = "/(G.+)\S/";
+  $regex2 = "/(G\S.+a)/";
   if(preg_match($regex2, $states, $statesArray1)) {
     echo "<p>$statesArray1[0]</p>";
   }
