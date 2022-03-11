d254f763e46cac7bc49ef48cd8e5939799215a93
Mike <shpoopdy@gmail.com>
2022-03-10 17:29:06 -0800

    move regex

--- findstates.php
+++ findstates.php
@@ -2,7 +2,7 @@
   $states = "California Georgia Indiana Louisiana Minnesota Texas Virginia";
   $statesArray1 = array();
 
-  $regex = "/\b(nia)$/i";
+  $regex = "/(nia)$\b/i";
   if(preg_match($regex, $states, $statesArray1)) {
     echo "<p>$statesArray1[0]</p>";
   }
