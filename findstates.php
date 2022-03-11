fd9c264be675d1e01c7afe1c86b8e05ae3f095d4
Mike <shpoopdy@gmail.com>
2022-03-10 17:55:03 -0800

    Revert again

--- findstates.php
+++ findstates.php
@@ -2,7 +2,7 @@
   $states = "California Georgia Indiana Louisiana Minnesota Texas Virginia";
   $statesArray1 = array();
 
-  $regex = "/(\S+)nia$/i";
+  $regex = "/(.+nia)/i";
   if(preg_match($regex, $states, $statesArray1)) {
     echo "<p>$statesArray1[0]</p>";
   }
