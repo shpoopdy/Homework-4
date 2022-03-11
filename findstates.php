50b20723a63fb8c5279ef2a174f1d62a0aa404cc
Mike <shpoopdy@gmail.com>
2022-03-10 16:36:38 -0800

    Create findstates.php

---
+++ findstates.php
@@ -0,0 +1,9 @@
+<?p
+  $states = "California Georgia Indiana Louisiana Minnesota Texas Virginia";
+  $statesArray1 = array();
+
+  $regex = /[nia]*/;
+  if(preg_match($regex, $states, $statesArray1[0])) {
+    echo <p>$statesArray1[0]</p>;
+  }
+?>
